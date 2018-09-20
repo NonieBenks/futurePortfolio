d1c9df416969ae96a4409b6be05792b3b7d2f93f
Nataliia Benko <33394119+NonieBenks@users.noreply.github.com>
2018-09-20 22:00:52 +0300

    Some updates
    Blur background on button hover.

--- portfolio.js
+++ portfolio.js
@@ -1,16 +1,19 @@
-/* $(document).ready(function() {
-  $('#play-video').on('click', function(ev) {
- 
-    $("#video")[0].src += "&autoplay=1";
-    ev.preventDefault();
- 
-  });
-});
-*/
-/*
-function myEnterFunction() {
-    var btnEnter = document.getElementById("checkbtn");
+ document.getElementById("checkbtn").addEventListener("over", overButton);
+document.getElementById("checkbtn").addEventListener("over", btnRise);
 
+function overButton() {
+  var picture = document.getElementById('white'); 
+  picture.style.filter='blur(10px)';
+  picture.style.transitionDuration='0.4s';
 
 }
-*/
+function btnRise() {
+  var exceptBtn =
+  document.getElementById('pageElements');
+  exceptBtn.style.color='red';
+}
+function resetBlur() {
+    var picture = document.getElementById('white'); 
+    picture.style.filter='blur(1px)';
+    picture.style.transitionDuration='0.4s';
+}
